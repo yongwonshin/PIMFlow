184f05b13c39e7b7d5baab5e93c7de0e4ca60bd5
sungjun <allencho1222@gmail.com>
2022-11-29 11:05:57 +0000

    comment out

--- pipeline/ALL.sh
+++ pipeline/ALL.sh
@@ -5,8 +5,8 @@ KERNEL_LAUNCH_LATENCY=$3
 N_CHANNEL=$4
 
 for (( i = 1; i < 4; i = i + 1)) ; do
-  # python3 extract_layers.py --model=$MODEL --n_channel=$N_CHANNEL
-  # python3 run --trace --gpgpusim_config=$GPU --model=$MODEL --pipeline=$i --kernel_launch_latency=$KERNEL_LAUNCH_LATENCY --n_channel=$N_CHANNEL
+  python3 extract_layers.py --model=$MODEL --n_channel=$N_CHANNEL
+  python3 run --trace --gpgpusim_config=$GPU --model=$MODEL --pipeline=$i --kernel_launch_latency=$KERNEL_LAUNCH_LATENCY --n_channel=$N_CHANNEL
   python3 run --simulate --gpgpusim_config=$GPU --model=$MODEL --pipeline=$i --kernel_launch_latency=$KERNEL_LAUNCH_LATENCY --n_channel=$N_CHANNEL
   python3 run --stat --gpgpusim_config=$GPU --model=$MODEL --pipeline=$i --kernel_launch_latency=$KERNEL_LAUNCH_LATENCY --n_channel=$N_CHANNEL
   sh clean.sh $MODEL $i $N_CHANNEL
