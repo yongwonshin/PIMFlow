899779716a03b42a9a234425e5a188fdca6586a5
Yongwon Shin <ywshin@postech.ac.kr>
2023-02-13 21:04:16 +0900

    fix minor mistake

--- docker/install.sh
+++ docker/install.sh
@@ -28,7 +28,7 @@ cd "$RAM_DIR"
 # Generate binary file: $RAM_DIR/ramulator
 make -j
 cd "$HOME"
-git clone -b https://github.com/yongwonshin/PIMFlow.git
+git clone -b "$GIT_BRANCH" https://github.com/yongwonshin/PIMFlow.git
 PIMFLOW_DIR="$HOME/PIMFlow"
 cd "$PIMFLOW_DIR"
 pip install -e .
