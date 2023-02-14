a7f97ddfe05891ff44f9183dbbe7a0a3b745c470
Yongwon Shin <ywshin@postech.ac.kr>
2023-02-14 21:06:21 +0900

    add python package & reorder conv_only stat

--- solve/stat.sh
+++ solve/stat.sh
@@ -3,9 +3,9 @@ MODEL=$1
 N_CHANNEL=$2
 STAGE=$3
 
-python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=1 --stage=$STAGE
 python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=1 --ramulator_disable_gwrite_latency_hiding --stage=$STAGE
-python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=2 --stage=$STAGE
+python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=1 --stage=$STAGE
 python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=2 --ramulator_disable_gwrite_latency_hiding --stage=$STAGE
-python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=4 --stage=$STAGE
+python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=2 --stage=$STAGE
 python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=4 --ramulator_disable_gwrite_latency_hiding --stage=$STAGE
+python3 stat.py --model=$MODEL --pipeline=all --n_channel=$N_CHANNEL --n_gwrite=4 --stage=$STAGE
